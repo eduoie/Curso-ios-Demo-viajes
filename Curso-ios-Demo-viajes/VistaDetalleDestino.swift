9c986ce69028e718718a2e03e68368c57e31b625
Eduardo <eduardo@microsoft.com>
2026-01-30 10:30:12 +0100

    Demo de vistas de un viajero y varios destinos editables

---
+++ Curso-ios-Demo-viajes/VistaDetalleDestino.swift
@@ -0,0 +1,18 @@
+//
+//  VistaDetalleDestino.swift
+//  Curso-ios-Demo-viajes
+//
+//  Created by Equipo 2 on 29/1/26.
+//
+
+import SwiftUI
+
+struct VistaDetalleDestino: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VistaDetalleDestino()
+}
---
+++ Curso-ios-Demo-viajes/modelos/Destino.swift
@@ -0,0 +1,8 @@
+//
+//  Destino.swift
+//  Curso-ios-Demo-viajes
+//
+//  Created by Equipo 2 on 29/1/26.
+//
+
+import Foundation
